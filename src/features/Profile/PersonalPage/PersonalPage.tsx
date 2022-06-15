d4704cb94865d0cb11f9e41f03cea6da34c18fe3
Dzmitry <maddal.privod@gmail.com>
2022-06-15 16:25:52 +0300

    Refactoring

--- src/features/Profile/PersonalPage/PersonalPage.tsx
+++ src/features/Profile/PersonalPage/PersonalPage.tsx
@@ -1,6 +1,9 @@
 import s from './PersonalPage.module.scss'
 import avatar from '../../../assets/images/user.svg'
 import {Input} from '../../../components/Input/Input';
+import {useFormik} from 'formik';
+import {Button} from '../../../components/Button/Button';
+import React, {useState} from 'react';
 
 export const PersonalPage = () => {
 
@@ -12,6 +15,41 @@ export const PersonalPage = () => {
         {label: 'Город',},
         {label: 'Мобильный телефон',},
     ]
+const [data, setData] = useState(
+    {label: 'Имя'}
+)
+
+    const formik = useFormik({
+        initialValues: {
+            firstName: data.label,
+            lastName: '',
+            middleName: '',
+            country: '',
+            city: '',
+            phone: '',
+            password: '',
+            repeatPassword: '',
+        },
+        validate: (values) => {
+            const errors: FormikErrorType = {};
+            if (!values.firstName) {
+                errors.name = 'Required';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.firstName)) {
+                errors.name = 'Invalid email address';
+            }
+            if (!values.password) {
+                errors.password = 'Password is required';
+            } else if (values.password.length < 7) {
+                errors.password = 'Password should be more 7 symbols';
+            }
+            return errors;
+        },
+        onSubmit: values => {
+            //dispatch(login(values))
+            //formik.resetForm()
+            //setData(values)
+        },
+    })
 
     return (
         <div className={s.wrapper}>
@@ -19,15 +57,28 @@ export const PersonalPage = () => {
             <span>Здравствуй, Захар!</span>
             <hr/>
             <div>Основные данные</div>
-            {dataField.map((d, i) => <Input
-                    key={i + d.label}
-                    label={d.label}
-                />
-            )}
-            <hr/>
-            <div>Пароль</div>
-            <Input label={'Новый пароль'}/>
-            <Input label={'Подтверждение пароля'}/>
+            <form onSubmit={formik.handleSubmit}>
+                {dataField.map((d, i) => <Input
+                        key={i + d.label}
+                        label={d.label}
+                        type={d.label}
+                    />
+                )}
+                <hr/>
+                <div>Пароль</div>
+                <Input label={'Новый пароль'} type={'password'}/>
+                <Input label={'Подтверждение пароля'} type={'password'}/>
+                <Button type={'submit'}>
+                    Сохранить
+                </Button>
+            </form>
         </div>
     )
+}
+
+//types
+type FormikErrorType = {
+    name?: string
+    password?: string
+    rememberMe?: boolean
 }
